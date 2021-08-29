e913338902806997aee29cc97e112dbed8577c02
UberStreuneR <explainyourorder@gmail.com>
2021-08-29 18:52:22 +0300

    React Works!

---
+++ backend/landing/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.views.generic import View
+# Create your views here.
+
+
+class HomeView(View):
+    def get(self, *args, **kwargs):
+        return render(self.request, "index.html")
--- landing/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.views.generic import View
-# Create your views here.
-
-
-class HomeView(View):
-    def get(self, *args, **kwargs):
-        return render(self.request, "landing/home.html")
